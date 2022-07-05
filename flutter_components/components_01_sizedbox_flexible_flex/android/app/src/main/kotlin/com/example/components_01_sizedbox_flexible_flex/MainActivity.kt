3af7c8857958eeef5d6d27d7f4566a7fb1a690e6
Martin <martin.e.castro.88@gmail.com>
2022-07-05 17:42:34 -0300

    use of sizedbox, flex and flexible widget

---
+++ flutter_components/components_01_sizedbox_flexible_flex/android/app/src/main/kotlin/com/example/components_01_sizedbox_flexible_flex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.components_01_sizedbox_flexible_flex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_components/components_01_sizedbox_flexible_flex/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Flexible(flex: 2, child: Container(color: Colors.red)),
+          Flexible(flex: 3, child: Container(color: Colors.blue)),
+          Flexible(flex: 1, child: Container(color: Colors.green))
+        ],
+      ),
+    );
+  }
+}
