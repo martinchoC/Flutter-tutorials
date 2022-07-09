c468e07bac9b8eaa28aa1e904ad66bd78b494935
Martin <martin.e.castro.88@gmail.com>
2022-07-09 01:04:05 -0300

    separation of main in order to create horizontal and vertical lists

--- flutter_components/components_05_listview/lib/main.dart
+++ flutter_components/components_05_listview/lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:components_05_listview/mainWidgets/horizontalHomePage.dart';
+import 'package:components_05_listview/mainWidgets/verticalHomePage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,49 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> list_titles = <String>[
-    "Widget 1",
-    "Widget 2",
-    "Widget 3",
-    "Widget 4",
-    "Widget 5",
-    "Widget 6",
-    "Widget 7",
-    "Widget 8",
-    "Widget 9",
-    "Widget 10",
-    "Widget 11",
-    "Widget 12",
-    "Widget 13"
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ListView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: list_titles.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              color: Colors.blue,
-              child: ListTile(
-                title: Text(list_titles[index]),
-              ),
-            );
-          },
-        ),
-      ),
+      home: HorizontalHomePage(),
     );
   }
 }
