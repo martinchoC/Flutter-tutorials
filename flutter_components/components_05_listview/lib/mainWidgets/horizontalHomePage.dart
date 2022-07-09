0499b3975aa79e79cf8bc3475feac7d32fc3a2da
Martin <martin.e.castro.88@gmail.com>
2022-07-09 01:04:20 -0300

    horizontal list

---
+++ flutter_components/components_05_listview/lib/mainWidgets/horizontalHomePage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HorizontalHomePage extends StatefulWidget {
+  const HorizontalHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HorizontalHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HorizontalHomePage> {
+  List<String> list_titles = <String>[
+    "Widget 1",
+    "Widget 2",
+    "Widget 3",
+    "Widget 4",
+    "Widget 5",
+    "Widget 6",
+    "Widget 7",
+    "Widget 8",
+    "Widget 9",
+    "Widget 10",
+    "Widget 11",
+    "Widget 12",
+    "Widget 13"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.2,
+          child: ListView.builder(
+            padding: const EdgeInsets.all(10),
+            scrollDirection: Axis.horizontal,
+            itemCount: list_titles.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: Card(
+                  color: Colors.blue,
+                  child: ListTile(
+                    title: Text(list_titles[index]),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
