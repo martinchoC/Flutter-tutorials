3a038de2235468c92749fe61d46b230c14c3fe9c
Martin <martin.e.castro.88@gmail.com>
2022-07-16 16:24:26 -0300

    cleaning code

--- flutter_components/components_07_navigating_between_screens_widgets/lib/main.dart
+++ flutter_components/components_07_navigating_between_screens_widgets/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:components_07_navigating_between_screens_widgets/pages/firstPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,23 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FirstPageWidget(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
