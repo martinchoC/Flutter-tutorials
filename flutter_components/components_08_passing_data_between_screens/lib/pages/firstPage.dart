a34a5fae0e711f1f4a08fb6f26714721a68a4a72
Martin <martin.e.castro.88@gmail.com>
2022-07-22 21:43:27 -0300

    passing data

--- flutter_components/components_08_passing_data_between_screens/lib/pages/firstPage.dart
+++ flutter_components/components_08_passing_data_between_screens/lib/pages/firstPage.dart
@@ -1,33 +1,41 @@
+import 'package:components_08_passing_data_between_screens/pages/secondPage.dart';
+
 import '../widgetArguments.dart';
 
 import 'package:flutter/material.dart';
 
+//1. widget button to go to next screen
 class FirstPageWidget extends StatelessWidget {
   const FirstPageWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('First Page')),
-      body: Center(
-        child: TextButton(
-          style: TextButton.styleFrom(
-              primary: Colors.white,
-              elevation: 2,
-              backgroundColor: Colors.blueAccent),
-          child: const Text(
-            'Go to page 2',
-          ),
-          onPressed: () {
-            //go to second page
-            Navigator.pushNamed(
-              context,
-              '/secondPage',
-              arguments: WidgetArguments("Going to Screen 2", 2),
-            );
-          },
-        ),
-      ),
-    );
+    return const Scaffold(body: Center(child: ButtonScreen2()));
   }
+}
+
+class ButtonScreen2 extends StatelessWidget {
+  const ButtonScreen2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          _navigateToSecondScreenAndWaitValue(context);
+        },
+        child: const Text('Go to widget 2'));
+  }
+}
+
+//2. method to start secondPage widget and waits for the result
+//async becaus we must wait the result
+_navigateToSecondScreenAndWaitValue(BuildContext context) async {
+  //3. create widget in /lib/pages
+  //5. import widget
+  final result = await Navigator.push(
+      context, MaterialPageRoute(builder: (context) => SecondPageWidget()));
+  //6. show result from secondPage widget with a snackBar/text
+  ScaffoldMessenger.of(context)
+    ..removeCurrentSnackBar()
+    ..showSnackBar(SnackBar(content: Text('$result')));
 }
--- flutter_components/components_08_passing_data_between_screens/lib/pages/secondPage.dart
+++ flutter_components/components_08_passing_data_between_screens/lib/pages/secondPage.dart
@@ -1,27 +1,26 @@
 import 'package:components_08_passing_data_between_screens/widgetArguments.dart';
 import 'package:flutter/material.dart';
 
+//4. implement the SecondPage widget logic
 class SecondPageWidget extends StatelessWidget {
   const SecondPageWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final arguments =
-        ModalRoute.of(context)!.settings.arguments as WidgetArguments;
     return Scaffold(
-      appBar: AppBar(title: Text(arguments.title)),
+      appBar: AppBar(title: const Text('Second Page')),
       body: Center(
         child: TextButton(
           style: TextButton.styleFrom(
               primary: Colors.white,
               elevation: 2,
               backgroundColor: Colors.blueAccent),
-          child: Text(
-            'To home from ${arguments.number}',
+          child: const Text(
+            'Go to main screen',
           ),
           onPressed: () {
             //go to main page
-            Navigator.pop(context);
+            Navigator.pop(context, 'Return from second page');
           },
         ),
       ),
