19b9641c672f9beadc4b19cf03fac88eb86c850a
Martin <martin.e.castro.88@gmail.com>
2022-12-31 20:16:27 -0300

    slider

---
+++ 04_advanced_flutter_components/advanced_components_02_slider/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _currentValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(children: [
+        Slider(
+          value: _currentValue,
+          min: 0,
+          max: 10,
+          label: _currentValue.round().toString(),
+          onChanged: ((value) {
+            setState(() {
+              _currentValue = value;
+            });
+          }),
+        ),
+        Text("${_currentValue.round()}")
+      ]),
+    );
+  }
+}
