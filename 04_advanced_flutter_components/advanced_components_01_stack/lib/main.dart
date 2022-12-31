b2af3c8c4cca6f57b4b207945cca891edb9c7f84
Martin <martin.e.castro.88@gmail.com>
2022-12-31 19:03:06 -0300

    stack

---
+++ 04_advanced_flutter_components/advanced_components_01_stack/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Stack(
+          fit: StackFit.loose,
+          children: [
+            Container(
+              color: Colors.red,
+              child: SizedBox(
+                width: 200,
+                height: 200,
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: SizedBox(
+                width: 150,
+                height: 150,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
