abfd77199f85273719fad3de138f63b1d936f843
Martin <martin.e.castro.88@gmail.com>
2023-01-05 19:14:01 -0300

    SimpleDialog, AlertDialog, SnackBar

--- 04_advanced_flutter_components/advanced_components_03_dialog/lib/main.dart
+++ 04_advanced_flutter_components/advanced_components_03_dialog/lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+enum options { SI, NO, QUIZA }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -29,11 +31,113 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Simple dialog
+  String _optionSelected = "Presioná acá";
+  void _setSelectedOption(String valueSelected) {
+    setState(() {
+      _optionSelected = valueSelected;
+    });
+  }
+
+  Future<void> _showDialog() async {
+    switch (await showDialog<options>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          backgroundColor: Colors.grey,
+          titlePadding: EdgeInsets.all(10),
+          title: Text(
+            "Simple dialog. Te gusta programar?",
+            style: TextStyle(color: Colors.white),
+          ),
+          children: [
+            SimpleDialogOption(
+              child: Text(
+                "Sí",
+                style: TextStyle(color: Colors.white70),
+              ),
+              onPressed: () => Navigator.pop(context, options.SI),
+            ),
+            SimpleDialogOption(
+              child: Text(
+                "No",
+                style: TextStyle(color: Colors.white70),
+              ),
+              onPressed: () => Navigator.pop(context, options.NO),
+            ),
+            SimpleDialogOption(
+              child: Text(
+                "Quizá",
+                style: TextStyle(color: Colors.white70),
+              ),
+              onPressed: () => Navigator.pop(context, options.QUIZA),
+            ),
+          ],
+        );
+      },
+    )) {
+      case options.SI:
+        _setSelectedOption("Si");
+        break;
+      case options.NO:
+        _setSelectedOption("No");
+        break;
+      case options.QUIZA:
+        _setSelectedOption("Quizá");
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return SafeArea(
+      child: Scaffold(
+        body: Align(
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //Simple dialog
+              ElevatedButton(
+                onPressed: _showDialog,
+                child: Text(_optionSelected),
+              ),
+              //AlertDialog
+              ElevatedButton(
+                child: Text("AlertDialog"),
+                onPressed: (() => showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Cuidado!"),
+                          content: Text("Querés salir de la app?"),
+                          actions: [
+                            TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("Si"))
+                          ],
+                        );
+                      },
+                    )),
+              ),
+              //SnackBar
+              Builder(builder: (context) {
+                return ElevatedButton(
+                  child: Text("SnackBar"),
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Row(children: [
+                        Icon(Icons.handshake),
+                        Text("Hola, soy un SnackBar"),
+                      ]),
+                    ));
+                  },
+                );
+              }),
+            ],
+          ),
+        ),
       ),
     );
   }
