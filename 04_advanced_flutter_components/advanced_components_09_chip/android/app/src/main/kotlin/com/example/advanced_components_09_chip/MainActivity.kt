7cf87e47e963a091cdb0646158cb85dcd0e5ea23
Martin <martin.e.castro.88@gmail.com>
2023-01-15 16:08:31 -0300

    Chip (tag)

---
+++ 04_advanced_flutter_components/advanced_components_09_chip/android/app/src/main/kotlin/com/example/advanced_components_09_chip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.advanced_components_09_chip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 04_advanced_flutter_components/advanced_components_09_chip/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        //chip
+        child: Chip(
+            elevation: 10,
+            labelPadding: EdgeInsets.all(10),
+            avatar: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Text(
+                "M",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            label: Text("Música")),
+      ),
+    );
+  }
+}
